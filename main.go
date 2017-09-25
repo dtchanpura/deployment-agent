6ac92488f2124d994a539b8089997cf7b63a0750
Darshil Chanpura <dtchanpura@gmail.com>
2017-09-26 00:12:26 +0530

    Update: Added an option for port and gin server

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	servePtr := flag.Bool("serve", false, "Use this to serve and start listener")
 	repoPtr := flag.String("repopath", "", "Path to Repository")
 	namePtr := flag.String("name", "", "Name of configuration")
+	serverPortPtr := flag.Int("port", 8080, "Port to listen on")
 
 	flag.Parse()
 	if *addPtr {
@@ -23,7 +24,7 @@ func main() {
 		AddConfiguration(*namePtr, *repoPtr, "")
 	} else if *servePtr {
 		fmt.Println("Serving...")
-
+		StartServer(*serverPortPtr)
 	} else {
 		fmt.Println("Atleast add or serve flag is required.")
 	}
--- manage/server.go
+++ manage/server.go
@@ -1,1 +1,20 @@
 package manage
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"fmt"
+)
+func StartServer(port int) {
+	gin.SetMode(gin.ReleaseMode)
+	router := gin.Default()
+	router.GET("/repository/:reponame/:token", func(c *gin.Context) {
+		reponame := c.Param("reponame")
+		token := c.Param("token")
+		c.Status(http.StatusOK)
+		fmt.Println(reponame, token)
+
+	})
+	addr := fmt.Sprintf(":%v", port)
+	router.Run(addr)
+}
