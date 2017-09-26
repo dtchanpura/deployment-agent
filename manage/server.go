01a081d15c279b92635ba1569d328ad4c8126cc3
Darshil Chanpura <dtchanpura@gmail.com>
2017-09-27 00:03:37 +0530

    Update: Added host in server and some validation function calls

--- manage/server.go
+++ manage/server.go
@@ -4,17 +4,25 @@ import (
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"fmt"
+	"log"
 )
-func StartServer(port int) {
+
+func StartServer(host string, port int) {
 	gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	router.GET("/repository/:reponame/:token", func(c *gin.Context) {
-		reponame := c.Param("reponame")
+		repoName := c.Param("reponame")
 		token := c.Param("token")
 		c.Status(http.StatusOK)
-		fmt.Println(reponame, token)
+		//fmt.Println(reponame, token)
+		result := validateToken(repoName, token)
+		if result {
+			c.Writer.Write([]byte("Valid"))
+		} else {
+			c.Writer.Write([]byte("Invalid"))
+		}
 
 	})
-	addr := fmt.Sprintf(":%v", port)
-	router.Run(addr)
+	addr := fmt.Sprintf("%s:%v", host, port)
+	log.Fatalln(router.Run(addr))
 }
