cff55dbedaad0396d3e2f1594f5a46d98607fe52
Darshil Chanpura <dtchanpura@gmail.com>
2017-09-27 10:00:05 +0530

    Update: Server to handle pull and execute hook logic.

--- manage/server.go
+++ manage/server.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 	"fmt"
 	"log"
+	"gopkg.in/src-d/go-git.v4"
 )
 
 func StartServer(host string, port int) {
@@ -17,12 +18,30 @@ func StartServer(host string, port int) {
 		//fmt.Println(reponame, token)
 		result := validateToken(repoName, token)
 		if result {
-			c.Writer.Write([]byte("Valid"))
+			var isUpToDate bool
+			c.Writer.Write([]byte("Token Valid\n"))
+			repo := findRepository(repoName)
+			err := PullRepository(repo.Path, repo.RemoteName)
+			if err != nil {
+				log.Printf("Error in Pulling: %v\n", err)
+				if err == git.NoErrAlreadyUpToDate {
+					isUpToDate = true
+				}
+			}
+			if !isUpToDate {
+				err = ExecuteHook(repo.PostHookPath)
+				if err != nil {
+					log.Printf("Error in Executing Hook: %v\n", err)
+				}
+			} else {
+				c.Writer.Write([]byte("Repository already up-to-date"))
+			}
 		} else {
 			c.Writer.Write([]byte("Invalid"))
 		}
 
 	})
 	addr := fmt.Sprintf("%s:%v", host, port)
+	log.Println("Server started at", addr)
 	log.Fatalln(router.Run(addr))
 }
