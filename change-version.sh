5184df46f76f91c17473785e2d4e14173a43f0ac
Darshil Chanpura <dtchanpura@gmail.com>
2018-05-13 12:22:44 +0530

    Update: version 0.4.0 with changed cmd from dep-agent to deployment-agent

---
+++ change-version.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+function print_usage () {
+	echo "  Usage: $0 -v v1.0.0"
+	echo
+	echo "  -v    --set-version version	for changing the version"
+	echo "  -h    --help				for displaying this help"
+	echo
+	exit 0
+}
+
+currentVersion=$(grep 'version = ' cmd/version.go | awk -F'"' '{print $2}')
+
+if [[ $# -eq 0 ]]; then
+    print_usage
+fi
+
+while [[ $# -gt 0 ]]
+do
+	key="$1"
+
+	case $key in
+		-v|--set-version)
+			VERSION="$2"
+			shift 2
+			;;
+		-h|--help)
+			print_usage
+			shift # past argument
+			;;
+	esac
+done
+
+function updateVersion() {
+	sed -i.bak "s/version = \".*\"/version = \"${VERSION}\"/g" cmd/version.go
+	sed -i.bak "s/buildDate = \".*\"/buildDate = \"$(date +"%Y-%m-%d %H:%M:%S %Z")\"/g" cmd/version.go
+	rm cmd/version.go.bak
+}
+
+if [ "X"$VERSION != "X" ]; then
+	updateVersion
+fi
